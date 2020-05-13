1f71e9384929d6e117e8e49b4b4872cc7b98b90d
kanoshinnosuke <yushph@gmail.com>
2020-05-13 22:26:27 +0900

    finish online page

--- src/components/HeroOnline.js
+++ src/components/HeroOnline.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import { useIntl, FormattedMessage } from "gatsby-plugin-intl"
 //
-import bgImage from '../img/bg-1.jpg'
+import bgImage from '../img/bg-2.svg'
 import Button from '../elements/Button'
 import DotsAndLines from '../components/DotsAndLines'
 
 
 const BackgroundImage = styled.section`
-    background-color: #e49356;
-    /* background-image: url(${bgImage}); */
-    background-position: center;
+    /* background-color: #e49356; */
+    background-image: url(${bgImage});
+    background-position: right;
     background-size: cover;
     height: 100vh;
     display: flex;
@@ -21,17 +21,17 @@ const HeroOnline = () => {
     const intl = useIntl()
     return (
         <BackgroundImage className="section">
-            <DotsAndLines />
+            {/* <DotsAndLines /> */}
 
             
             <div className="container">
                 <div className="columns is-vcentered">
-                    <div className="hero-text column has-text-centered">
-                        <h1 className="title color-white is-size-1">
+                    <div className="hero-text column has-text-centered margin-bottom-7">
+                        <h1 className="title is-size-1">
                             <span className="inline-block">超本気 IT留学</span>
-                            <span className="inline-block">オンライン</span>
+                            <span className="inline-block">×オンライン</span>
                         </h1>
-                        <h2 className="subtitle is-6 color-white has-text-weight-bold">
+                        <h2 className="subtitle is-6 has-text-weight-bold">
                             <span className="inline-block">
                                 <FormattedMessage id="hero-subtitle04" />
                             </span>
@@ -39,13 +39,13 @@ const HeroOnline = () => {
                                 <FormattedMessage id="hero-subtitle05" />
                             </span>
                         </h2>
-                        <h2 className="subtitle color-white">
+                        <h2 className="subtitle">
                             
                             <span className="inline-block">
                                 コロナに負けるな！&nbsp;
                             </span>
                             <span className="inline-block">
-                                特別価格キャンペーン実施中！
+                                特別価格キャンペーン中
                             </span>
                         </h2>
                         <Button 
